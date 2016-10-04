97da3a5e656753db598894679784fdadf1407124
Anthony <Anthony@LAPTOP-2EAHK059>
2016-10-04 09:59:48 -0600

    4 Oct 2016 Created Project

---
+++ Gladiator/src/gladiator/Character.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gladiator;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Character implements Serializable {
+    public Character(){ }
+    public string getName() {return _name; }
+    private string _name;
+    
+}
