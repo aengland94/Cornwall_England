42a335437887036cba9c1a9915da90cb104b1055
Anthony <Anthony@LAPTOP-2EAHK059>
2016-10-06 10:55:49 -0600

    6 Oct 2016 ta1 main and player

---
+++ teamProject1/src/unique/Player.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unique;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Anthony
+ */
+public class Player implements Serializable{
+    private String _name;
+    private double _bestTime;
+
+    public Player() {
+    }
+    
+    public String getName() { return _name;}
+    public void setName(String name) {_name = name; }
+    public double getBestTime() { return _bestTime; }
+    public void setBestTime(double bestTime) { _bestTime = bestTime; }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + Objects.hashCode(this._name);
+        hash = 41 * hash + (int) (Double.doubleToLongBits(this._bestTime) ^ (Double.doubleToLongBits(this._bestTime) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this._bestTime) != Double.doubleToLongBits(other._bestTime)) {
+            return false;
+        }
+        if (!Objects.equals(this._name, other._name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "_name=" + _name + ", _bestTime=" + _bestTime + '}';
+    }
+    
+}
