9df860d3bb635cea2d17379563cce27447259b52
Camdey <Camdey@10.38.165.254>
2016-10-07 11:12:58 -0600

    10-7-16 made fight

---
+++ Gladiator/src/byui/cit260/gladiator/model/Fight.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.gladiator.model;
+
+/**
+ *
+ * @author Camdey
+ */
+public class Fight {
+    private int _fighteq;
+    public int getFighteq(){ return _fighteq; }
+    public void setFighteq(int fighteq) { _fighteq = fighteq; } 
+}
