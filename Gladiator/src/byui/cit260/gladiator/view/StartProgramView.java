b61c85eff200f3bde230b195eae14e5be6f10807
Anthony <Anthony@LAPTOP-2EAHK059>
2016-10-31 09:45:21 -0600

    31 Oct 2016 created more of the view layer

--- Gladiator/src/byui/cit260/gladiator/view/StartProgramView.java
+++ Gladiator/src/byui/cit260/gladiator/view/StartProgramView.java
@@ -14,13 +14,31 @@ import java.io.Serializable;
  * @author Anthony
  */
 public class StartProgramView implements Serializable{
-    public StartProgramView() { }
-    public void displayStartProgramView() {
-        System.out.println("WELCOME TO GLADIATOR");
-        System.out.print("Welcome Warrior! What should we call you?");
-        String myName = System.console().readLine();
+    public StartProgramView() { 
+        System.out.println("Welcome Warrior! What should we call you?");
         
-        Player player = GameControl.createPlayer(myName);
+        displayBanner();
+    }
+    private void displayBanner() {
+        System.out.println("WELCOME TO GLADIATOR!\n"
+        + "This is a game where you will be fighting your way through\n"
+        + "a collecium to overthrow the current Ceaser of Rome to free\n"
+        + "yourself and your people from the bondage you are in as a\n"
+        + "Roman Gladiator.\n\n"
+        + "You will start at the bottom of the collecium. You will have\n"
+        + "to search rooms, fight Bosses, and complete different objectives\n"
+        + "each floor to be able to move onto the next floor. Each floor will\n"
+        + "progressively get harder in diffuculty until you finally are able\n"
+        + "to reach the top. There you will be given the oppertunity to fight\n"
+        + "Ceaser himself. Beating Ceaser will result in freedom for you and\n"
+        + "your people as well as the winning of the Game!\n\n");
+    }
+    public void displayStartProgramView() {
+        
+        
+        //String myName = System.console().readLine();
+        
+        //Player player = GameControl.createPlayer(myName);
         //System.out.println("Hello " + player.getName());
     }
 }
