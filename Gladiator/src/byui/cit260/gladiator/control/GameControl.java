5bd7da32b37a82387895c540f6df94e4bf5e25ca
Anthony <Anthony@LAPTOP-2EAHK059>
2016-11-08 09:25:16 -0700

    8 Nov 2016 made different rooms an option

--- Gladiator/src/byui/cit260/gladiator/control/GameControl.java
+++ Gladiator/src/byui/cit260/gladiator/control/GameControl.java
@@ -5,8 +5,14 @@
  */
 package byui.cit260.gladiator.control;
 
+import byui.cit260.gladiator.model.BossRoom;
+import byui.cit260.gladiator.model.DiningRoom;
+import byui.cit260.gladiator.model.Fountain;
+import byui.cit260.gladiator.model.Hallway;
+import byui.cit260.gladiator.model.Kitchen;
 import byui.cit260.gladiator.model.Player;
 import byui.cit260.gladiator.model.Room;
+import byui.cit260.gladiator.model.Stairway;
 import gladiator.Gladiator;
 import java.io.Serializable;
 
@@ -29,7 +35,63 @@ public class GameControl implements Serializable{
         return player;
     }
     public static Room createRoom(){
-        Room room = new Room();
+        Room room;
+        
+        int num =Control.randInt(1, 4);
+        
+        String type;
+        
+        if(num == 2){
+            type = "BOSS ROOM";
+        }
+        else if (num == 3){
+            type = "FOUNTAIN";
+        }
+        else if (num == 4){
+            type = "STAIRWAY";
+        }
+        else{
+            type = "OTHER";
+        }
+        
+        switch(num){
+            case 1:
+                num = Control.randInt(1, 6);
+                switch(num){
+                    case 1:
+                        room = new Kitchen();
+                        break;
+                    case 2:
+                        room = new DiningRoom();
+                        break;
+                    case 3:
+                        room = new Hallway();
+                        break;
+                    case 4:
+                        room = new Room();
+                        break;
+                    case 5:
+                        room = new Room();
+                        break;
+                    case 6:
+                        room = new Room();
+                        break;
+                    default:
+                        room = new Room();
+                }
+                break;
+            case 2:
+                room = new BossRoom();
+                break;
+            case 3:
+                room = new Fountain();
+                break;
+            case 4:
+                room = new Stairway();
+                break;
+            default:
+                room = new Room();
+        }
         Gladiator.setCurrentRoom(room);
         
         return room;
--- Gladiator/src/byui/cit260/gladiator/model/Weapon.java
+++ Gladiator/src/byui/cit260/gladiator/model/Weapon.java
@@ -13,26 +13,5 @@ import java.util.Random;
  * @author Camdey
  */
 public class Weapon extends Item implements Serializable{
-    
-        int weapon1 = 2;
-        int w1 = 0;
-        int ww1 = 6;
-        int rando = randInt(w1,ww1);
-        int damage = rando + weapon1;
-    
-    
-        public static int randInt(int min, int max) {
-            
-	if (min > max) {
-		throw new IllegalArgumentException("max must be greater than min");
-	}
-        
-        Random r = new Random();
-
-        int randomNum = r.nextInt((max - min) + 1) + min;
-
-        return randomNum;
-}
-
-    
+    public Weapon() { }
 }
