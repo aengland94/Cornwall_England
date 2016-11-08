b553cb912909da1cf11f6a496ac1f8860157350c
Krissy <Krissy@10.38.166.135>
2016-11-08 09:55:07 -0700

    10/21/16 L08 Team Assignment (partial)

---
+++ Gladiator/src/byui/cit260/gladiator/view/ViewAbstractImp.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.gladiator.view;
+// not really sure what this view is suppose to do ~Krissy
+
+
+public class ViewAbstractImp extends ViewAbstract {
+
+    public ViewAbstractImp(String message) {
+        super(message);
+    }
+
+    @Override
+    public void display() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getInput() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean doAction(String value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
