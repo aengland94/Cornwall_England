160cd756e64b4eeca0dc23aac86882a6a896148c
Krissy <Krissy@10.38.165.174>
2016-11-10 10:04:35 -0700

    11/10/16 L08 Individual Assignment

---
+++ Gladiator/src/byui/cit260/gladiator/view/NextRoomView.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.gladiator.view;
+
+/**
+ *
+ * @author Krissy
+ */
+public interface NextRoomView { //extends View
+    
+//description of the room.    
+}
