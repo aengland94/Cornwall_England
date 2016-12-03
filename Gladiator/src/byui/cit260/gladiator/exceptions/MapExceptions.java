d85a4350b6367146bbf0a21c4d00eb05988201d8
Anthony <Anthony@LAPTOP-2EAHK059>
2016-12-02 20:27:02 -0700

    2 Dec 2016 exception crap

--- Gladiator/src/byui/cit260/gladiator/exceptions/MapExceptions.java
+++ Gladiator/src/byui/cit260/gladiator/exceptions/MapExceptions.java
@@ -9,6 +9,25 @@ package byui.cit260.gladiator.exceptions;
  *
  * @author Krissy
  */
-public class MapExceptions {
+public class MapExceptions extends Exception {
+
+    public MapExceptions() {
+    }
+
+    public MapExceptions(String message) {
+        super(message);
+    }
+
+    public MapExceptions(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MapExceptions(Throwable cause) {
+        super(cause);
+    }
+
+    public MapExceptions(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
     
 }
