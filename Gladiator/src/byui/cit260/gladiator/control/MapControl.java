69db18f3d9bb8e42d1205d930afb567cf672098e
Camdey <Camdey@10.38.165.254>
2016-12-02 21:01:53 -0700

    12/2/2016 Fight Stuff

--- Gladiator/src/byui/cit260/gladiator/control/MapControl.java
+++ Gladiator/src/byui/cit260/gladiator/control/MapControl.java
@@ -5,10 +5,28 @@
  */
 package byui.cit260.gladiator.control;
 
+import byui.cit260.gladiator.model.Map;
+import byui.cit260.gladiator.model.Player;
+
 /**
  *
  * @author Anthony
  */
 public class MapControl {
+    public int go(int move, Player player, Map map) { 
+        int x = 0;
+        if(move == 1){
+            if(player.getX() == 4){
+                player.setX(0);}
+            else {
+                player.setX(player.getX() + 1);}}
+        else if(move == -1){
+            if(player.getX() == 0){
+                player.setX(4);}
+            else {
+                player.setX(player.getX() - 1);}}
+        else{
+        System.out.println("Invalid Move");}
+        return x; }
     
 }
--- Gladiator/src/byui/cit260/gladiator/control/PlayerControl.java
+++ Gladiator/src/byui/cit260/gladiator/control/PlayerControl.java
@@ -15,21 +15,7 @@ import java.io.Serializable;
  * @author Camdey
  */
 public class PlayerControl extends CharacterControl implements Serializable{
-    private int x;
-    public int go(int move, Player player, Map map) { 
-        if(move == 1){
-            if(player.getX() == 4){
-                player.setX(0);}
-            else {
-                player.setX(player.getX() + 1);}}
-        else if(move == -1){
-            if(player.getX() == 0){
-                player.setX(4);}
-            else {
-                player.setX(player.getX() - 1);}}
-        else{
-        System.out.println("Invalid Move");}
-        return x; }
+
     
     
     
