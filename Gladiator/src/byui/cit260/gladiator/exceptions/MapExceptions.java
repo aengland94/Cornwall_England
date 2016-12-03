4c78c2abbd1fd483eea4b18b7933d410e45a53cf
Krissy <Krissy@Personal>
2016-12-02 20:42:26 -0700

    12/2/16 10 Team Assignment

--- Gladiator/src/byui/cit260/gladiator/exceptions/MapExceptions.java
+++ Gladiator/src/byui/cit260/gladiator/exceptions/MapExceptions.java
@@ -9,6 +9,25 @@ package byui.cit260.gladiator.exceptions;
  *
  * @author Krissy
  */
-public class MapExceptions {
+public class MapExceptions extends Exception {
+
+    public MapExceptions() {
+    }
+
+    public MapExceptions(String string) {
+        super(string);
+    }
+
+    public MapExceptions(String string, Throwable cause) {
+        super(string, cause);
+    }
+
+    public MapExceptions(Throwable cause) {
+        super(cause);
+    }
+
+    public MapExceptions(String string, Throwable cause, boolean bln, boolean bln1) {
+        super(string, cause, bln, bln1);
+    }
     
 }
--- Gladiator/src/byui/cit260/gladiator/model/Map.java
+++ Gladiator/src/byui/cit260/gladiator/model/Map.java
@@ -57,5 +57,4 @@ public class Map implements Serializable {
         return true;
     }
     
-    
-}
+    }
